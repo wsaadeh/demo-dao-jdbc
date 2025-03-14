c9c022228829a706033cd1c76393519d66eb661c
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-03-14 18:15:28 -0300

    SellerDaoJdbc and DaoFactory

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,9 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoJdbc;
+
+public class DaoFactory {
+    public static SellerDao createSellerDao(){
+        return new SellerDaoJdbc();
+    }
+}
