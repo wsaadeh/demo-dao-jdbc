fb33e96478e7d94bcae58c31d01824d8e12a1dfc
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-03-14 17:25:57 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,16 @@
 package application;
 
 import models.entities.Department;
+import models.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1,"Books");
         System.out.println(obj);
+
+        Seller seller = new Seller(21,"Bob","bob@gmail.com",new Date(),3000.0, obj);
+        System.out.println(seller);
     }
 }
