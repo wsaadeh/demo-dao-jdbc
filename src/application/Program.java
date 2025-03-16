f077ba9298e1dec1f53b0a2be48d07e8a1aa318c
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-03-15 21:26:17 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,5 +40,12 @@ public class Program {
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
 
+        System.out.println();
+        System.out.println("=== Test 5:Seller update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed!");
+
     }
 }
--- src/model/dao/impl/SellerDaoJdbc.java
+++ src/model/dao/impl/SellerDaoJdbc.java
@@ -35,35 +35,57 @@ public class SellerDaoJdbc implements SellerDao {
                     Statement.RETURN_GENERATED_KEYS
             );
 
-            st.setString(1,obj.getName());
-            st.setString(2,obj.getEmail());
+            st.setString(1, obj.getName());
+            st.setString(2, obj.getEmail());
             st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
-            st.setDouble(4,obj.getBaseSalary());
-            st.setInt(5,obj.getDepartment().getId());
+            st.setDouble(4, obj.getBaseSalary());
+            st.setInt(5, obj.getDepartment().getId());
 
             int rowAffect = st.executeUpdate();
 
-            if (rowAffect > 0){
+            if (rowAffect > 0) {
                 ResultSet rs = st.getGeneratedKeys();
-                if (rs.next()){
+                if (rs.next()) {
                     int id = rs.getInt(1);
                     obj.setId(id);
                 }
                 DB.closeResultSet(rs);
-            }else {
+            } else {
                 throw new DbException("Unexpected error! No rows affected!");
             }
 
         } catch (SQLException e) {
             throw new DbException(e.getMessage());
-        }finally {
+        } finally {
             DB.closeStatement(st);
         }
     }
 
     @Override
     public void update(Seller obj) {
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement(
+                    "UPDATE seller "
+                            + "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+                            + "WHERE Id = ? ",
+                    Statement.RETURN_GENERATED_KEYS
+            );
 
+            st.setString(1, obj.getName());
+            st.setString(2, obj.getEmail());
+            st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+            st.setDouble(4, obj.getBaseSalary());
+            st.setInt(5, obj.getDepartment().getId());
+            st.setInt(6,obj.getId());
+
+            st.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
